a9de6b381db930ac49df42baa476d33462f3feda
Sinthia-22 <sinthiasupti@gmail.com>
2023-10-24 12:33:38 +0600

    First Commit

---
+++ JS/script.js
@@ -0,0 +1,100 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active'); 
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+}
+
+
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+    loginForm.classList.toggle('active'); 
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navBar.classList.remove('active');
+}    
+
+
+
+let navBar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>
+{
+    navBar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');   
+}    
+
+window.onscroll = () =>
+{
+    navBar.classList.remove('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}    
+
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:
+    {
+        delay:5500,
+        disableOnInteraction:false,
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:
+    {
+        delay:5500,
+        disableOnInteraction:false,
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
